12a163686971e8d0f2376988d6d5746757816513
Natnael Samuel <natisami901@gmail.com>
2025-02-23 13:39:15 +0300

    Built the Hero Section

---
+++ Client/scripts.js
@@ -0,0 +1,11 @@
+function applyNow() {
+    window.location.href = 'https://peak-apply.paperform.co';
+}
+
+function learnMore() {
+    window.location.href = '#about';
+}
+
+function viewCriteria() {
+    window.location.href = '#join';
+}
